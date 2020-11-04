fb97a1179b527d871fb6804ea070b1be194537a8
ChevalierSoft <chevalierSoft@darbaland.online>
2020-11-04 21:34:16 +0100

    updating ft_strlen, using less registers

--- ft_strlen.s
+++ ft_strlen.s
@@ -3,12 +3,10 @@
 			section .text
 
 ft_strlen:	mov		rax, 0
-			movzx	r8, Byte[rdi]	; return (rax)
 
-while:		cmp		r8, 0			; while (r8)
+while:		cmp		Byte[rdi + rax], 0			; while (r8)
 			je		return
 			inc		rax
-			movzx	r8, Byte[rdi + rax]
 			jmp		while
 
 return:		ret
