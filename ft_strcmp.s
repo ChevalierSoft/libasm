dbde78a3369e89ca783c948a84437b65a7674a39
ChevalierSoft <chevalierSoft@darbaland.online>
2020-11-07 08:50:28 +0100

    cleaning a bit

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -3,7 +3,7 @@
 			section .text
 
 ft_strcmp:
-			mov		rax, 0				; res  = 0
+			mov		rax, 0
 
 while:
 			cmp		Byte[rdi + rax], 0
@@ -17,7 +17,7 @@ while:
 			jmp		while
 
 return:
-			movzx	r8, Byte[rdi + rax]; r9b = *(rdi + rax)
+			movzx	r8, Byte[rdi + rax]
 			movzx	r9, Byte[rsi + rax]
 			sub		r8, r9
 			mov		rax, r8
